f3d2162fa3297b062ae4cb6d1ced357d7d6d05a6
rahul26ur <rahul26ur@gmail.com>
2021-06-07 17:16:34 +0530

    pushing the entire project foler by rahul

---
+++ rah-rest-webservices/src/main/java/com/rah/rest/webservices/rahrestwebservices/RahRestWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rah.rest.webservices.rahrestwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahRestWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahRestWebservicesApplication.class, args);
+	}
+
+}
---
+++ rah-rest-webservices/src/test/java/com/rah/rest/webservices/rahrestwebservices/RahRestWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rah.rest.webservices.rahrestwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahRestWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
